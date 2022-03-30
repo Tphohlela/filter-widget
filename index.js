362a6ef1d91d16932758b53705f66dccd35bb827
Tphohlela <17thapho@gmail.com>
2022-03-30 13:40:11 +0200

    filtering

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 let brandsElem = document.querySelector('.make')
 let coloursElem = document.querySelector('.colours')
-
+let displayElem = document.querySelector('.displayCars')
 
 axios
-     .get('https://api-tutor.herokuapp.com/v1/cars')
-     .then(function(result){
-         console.log(result.data)
+    .get('https://api-tutor.herokuapp.com/v1/cars')
+    .then(function (result) {
+        console.log(result.data)
 
-         let brandList = [];
-         let colourList = [];
+        let brandList = [];
+        let colourList = [];
 
-         result.data.forEach(element => {
-             if(!brandList.includes(element.make)){
+        result.data.forEach(element => {
+            if (!brandList.includes(element.make)) {
                 brandList.push(element.make)
-             }
+            }
 
-             else if(!colourList.includes(element.color)){
+            else if (!colourList.includes(element.color)) {
                 colourList.push(element.color)
-             }
+            }
         })
 
         brandList.forEach(element => {
             const dropdown = document.createElement('option')
-            dropdown.innerHTML = `<option>
+            dropdown.innerHTML = `<option value="${element}">
         ${element}
             </option>          
             `
@@ -31,13 +31,57 @@ axios
 
         colourList.forEach(element => {
             const dropdown = document.createElement('option')
-            dropdown.innerHTML = `<option>
+            dropdown.innerHTML = `<option value="${element}">
         ${element}
             </option>          
             `
             coloursElem.appendChild(dropdown)
         })
+    })
 
+    const check = () => {
+        // console.log(brandsElem.value)
+        var templateString = document.querySelector('.entry-template').innerHTML;
+        let arr = [];
+       
+        axios
+    .get('https://api-tutor.herokuapp.com/v1/cars')
+    .then(function (result) {
+        console.log(result.data)
 
-     })
+        result.data.forEach(element => {
+            if (element.make == brandsElem.value && element.color == coloursElem.value) {
+            arr.push({
+                reg:`${element.reg_number}`,
+                colour:`${element.color}`,
+                brand:`${element.make}`
+                })   
+            }
 
+            else if(element.make == brandsElem.value && coloursElem.value == ''){
+                arr.push({
+                    reg:`${element.reg_number}`,
+                    colour:`${element.color}`,
+                    brand:`${element.make}`
+                    })  
+            }
+
+            else if(element.color == coloursElem.value && brandsElem.value == ''){
+                arr.push({
+                    reg:`${element.reg_number}`,
+                    colour:`${element.color}`,
+                    brand:`${element.make}`
+                    })  
+            }
+        })
+
+        let context = {
+            "car": arr,
+        };
+
+        console.log('sdfghj'+JSON.stringify(context)) 
+
+        let templateScript = Handlebars.compile(templateString); 
+            displayElem.innerHTML = templateScript(context);
+    })   
+    }
